9ea021aed2fe0109962d21cb4e7d51d1237ac907
Aaron Voelker <arvoelke@gmail.com>
2018-10-31 16:13:19 -0400

    WIP: Numba-compiled LIF neuron model with profiling test

--- nengo/neurons.py
+++ nengo/neurons.py
@@ -4,6 +4,7 @@ import logging
 import warnings
 
 import numpy as np
+from numba import njit
 
 from nengo.exceptions import SimulationError, ValidationError
 from nengo.params import Parameter, NumberParam, FrozenObject
@@ -480,6 +481,81 @@ class LIF(LIFRate):
         voltage[voltage < self.min_voltage] = self.min_voltage
         voltage[spiked_mask] = 0
         refractory_time[spiked_mask] = self.tau_ref + t_spike
+
+
+@njit
+def custom_clip(x, a, b):
+    """Numba-compiled version of np.clip."""
+    # np.clip is not supported by numba
+    # https://github.com/lmcinnes/umap/issues/84
+    # note: the out option is not supported here
+    y = np.empty_like(x)
+    for i in range(len(x)):
+        if x[i] < a:
+            y[i] = a
+        elif x[i] > b:
+            y[i] = b
+        else:
+            y[i] = x[i]
+    return y
+
+
+@njit
+def _lif_step_math(dt, J, spiked, voltage, refractory_time,
+                   tau_rc, tau_ref, min_voltage, amplitude):
+    """Numba-compiled version of LIF.step_math."""
+    # reduce all refractory times by dt
+    refractory_time -= dt
+
+    # compute effective dt for each neuron, based on remaining time.
+    # note that refractory times that have completed midway into this
+    # timestep will be given a partial timestep, and moreover these will
+    # be subtracted to zero at the next timestep (or reset by a spike)
+    delta_t = custom_clip(dt - refractory_time, 0, dt)
+
+    # update voltage using discretized lowpass filter
+    # since v(t) = v(0) + (J - v(0))*(1 - exp(-t/tau)) assuming
+    # J is constant over the interval [t, t + dt)
+    voltage -= (J - voltage) * np.expm1(-delta_t / tau_rc)
+
+    # determine which neurons spiked (set them to 1/dt, else 0)
+    spiked_mask = voltage > 1
+    spiked[:] = spiked_mask * (amplitude / dt)
+
+    # set v(0) = 1 and solve for t to compute the spike time
+    t_spike = dt + tau_rc * np.log1p(
+        -(voltage[spiked_mask] - 1) / (J[spiked_mask] - 1))
+
+    # set spiked voltages to zero, refractory times to tau_ref, and
+    # rectify negative voltages to a floor of min_voltage
+    voltage[voltage < min_voltage] = min_voltage
+    voltage[spiked_mask] = 0
+    refractory_time[spiked_mask] = tau_ref + t_spike
+
+
+class NumbaLIF(LIF):
+    """Numba-compiled version of the LIF model.
+
+    Parameters
+    ----------
+    tau_rc : float
+        Membrane RC time constant, in seconds. Affects how quickly the membrane
+        voltage decays to zero in the absence of input (larger = slower decay).
+    tau_ref : float
+        Absolute refractory period, in seconds. This is how long the
+        membrane voltage is held at zero after a spike.
+    min_voltage : float
+        Minimum value for the membrane voltage. If ``-np.inf``, the voltage
+        is never clipped.
+    amplitude : float
+        Scaling factor on the neuron output. Corresponds to the relative
+        amplitude of the output spikes of the neuron.
+    """
+
+    def step_math(self, dt, J, spiked, voltage, refractory_time):
+        return _lif_step_math(
+            dt, J, spiked, voltage, refractory_time,
+            self.tau_rc, self.tau_ref, self.min_voltage, self.amplitude)
 
 
 class AdaptiveLIFRate(LIFRate):
